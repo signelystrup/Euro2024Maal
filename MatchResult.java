18ddff3e3aed29ba58e43129f742bb64df84cc35
jazzyzoney <jaqtash22@gmail.com>
2024-08-27 10:37:26 +0200

    matchResult kode fra filen

--- MatchResult.java
+++ MatchResult.java
@@ -1,2 +1,16 @@
 public class MatchResult {
+
+    private String teams;
+    private List<String> goalScorers;
+
+    public MatchResult(String teams, List<String> goalScorers){
+
+
+    }
+
+    public List<String> getGoalScorers(){
+
+
+    }
+
 }
